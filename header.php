f1cdbe50033b356b75c4bf3498681ec10815b97d
omidbeheshtian <abravaranomidkish@gmail.com>
2021-06-26 19:12:57 +0430

    menu completed

--- header.php
+++ header.php
@@ -9,31 +9,41 @@
     <meta name="robots" content="noindex,nofollow">
     <meta name="keywords" content="abravaran omid kish, abravaran, abravaran omid, ابرآوران ,ابر آوران امید کیش ,شرکت ابرآوران,شرکت ابرآوران امید کیش">
     <meta name="description" content="طراحی حرفه ای ترین سایت ها و پشتیبانی بی نظیر از خدمات رایج ما است!">
+    <meta name="theme-color" content="#8E24AA">
     <link rel="icon" href="img/favicon.ico">
     <link rel="preload" href="assets/fonts/woff/IRANSansX-Regular.woff" as="font" type="font/woff" crossorigin> 
     <link rel="preload" href="assets/fonts/woff2/IRANSansX-Regular.woff2" as="font" type="font/woff2" crossorigin> 
     <link rel="preload" href="assets/fonts/woff/IRANSansX-Bold.woff" as="font" type="font/woff" crossorigin> 
     <link rel="preload" href="assets/fonts/woff2/IRANSansX-Bold.woff2" as="font" type="font/woff2" crossorigin> 
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <!-- menu header -->
     <header>
         <nav>
-            <li>
-                <ul>
-                    صفحه اصلی
-                </ul>
-                <ul>
-                    خدمات
-                </ul>
-                <ul>
-                    درباره ما
-                </ul>
-                <ul>
-                    تماس با ما
-                </ul>
-            </li>
+            <ul class="menu">>
+                <li>
+                    <a href="#">صفحه اصلی</a>
+                </li>
+                <li>
+                    <a href="#">درباره ما</a>
+                </li>
+                <li>
+                    <a href="#">خدمات<i class="fa fa-caret-down"></i></a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="#">طراحی سایت</a>
+                            </li>
+                            <li>
+                                <a href="#">سئو تخصصی</a>
+                            </li>
+                        </ul>
+                </li>
+                <li>
+                    <a href="#">تماس با ما</a>
+                </li>
+            </ul>
         </nav>
     </header>
 <!-- end of menu header -->
